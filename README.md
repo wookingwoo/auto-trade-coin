5902cd165674c7e4053f507cce33d3fa587da39d
JeongHeon Woo <contact@wookingwoo.com>
2021-04-29 02:57:10 +0900

    Add balance inquiry

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # auto-trade-coin
+
+
+https://github.com/sharebook-kr/pyupbit
+
+
+
+## Installation
+
+```sh
+pip install pyupbit
+```
+
+pyjwt 모듈을 필요로 합니다. (pyjwt >= 2.0)
+
+```sh
+pip install pyjwt
+```
---
+++ balanceInquiry.py
@@ -0,0 +1,14 @@
+import pyupbit
+import data.apiKey
+
+# 로그인
+access = data.apiKey.access 
+secret = data.apiKey.secret 
+upbit = pyupbit.Upbit(access, secret)
+
+# 잔고 조회
+print(upbit.get_balance("KRW-BTC")) # KRW - BTC 조회
+print(upbit.get_balance("KRW-ETH")) # KRW - ETH 조회
+print(upbit.get_balance("KRW-DOGE")) # KRW - DOGE 조회
+
+print(upbit.get_balance("KRW")) # 보유 현금 조회
