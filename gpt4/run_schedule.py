4bc372cba7ea9c63b4773c66847c1cc2a3056145
JeongHeon Woo <email@wookingwoo.com>
2024-03-12 04:30:44 +0900

    Update run_schedule.py

--- gpt4/run_schedule.py
+++ gpt4/run_schedule.py
@@ -7,8 +7,8 @@ from main import make_decision_and_execute
 
 send_slack_message("주식 자동매매 봇을 시작합니다. :robot_face:")
 make_decision_and_execute()
-schedule.every().minute.do(make_decision_and_execute)
-# schedule.every().hour.do(make_decision_and_execute)
+# schedule.every().minute.do(make_decision_and_execute)
+schedule.every().hour.do(make_decision_and_execute)
 
 while True:
     schedule.run_pending()
